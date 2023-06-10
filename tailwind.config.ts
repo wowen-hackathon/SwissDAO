8427dcab83434af1993629f3f92cc822dd266b7f
Olivier <olivier.winkler@icloud.com>
2023-06-10 15:03:20 +0200

    feat(app): add wowen header

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,9 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      header: '#2B3D00'
+    },
     extend: {},
   },
   plugins: [],
