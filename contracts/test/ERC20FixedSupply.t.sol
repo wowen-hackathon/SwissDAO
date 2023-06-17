37541424145c1e78cb1f8e56f65cce20479d9ec3
lukas <lukas@ibex.host>
2023-06-17 09:09:10 +0200

    Non-Working tests commited because package needs to be installed.

--- contracts/test/ERC20FixedSupply.t.sol
+++ contracts/test/ERC20FixedSupply.t.sol
@@ -1,57 +1,42 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@forge-std/Test.sol";
+import {console} from "@forge-std/console.sol";
+import {stdStorage, StdStorage, Test} from "@forge-std/Test.sol";
 
 import "../src/erc20/ERC20FixedSupply.sol";
 
-contract ERC20FixedSupplyTest is Test {
-    ERC20FixedSupply private token;
-    address private owner;
+contract ERC20FixedSupplyBaseSetup is ERC20FixedSupply, Test {
+    Utils internal utils;
+    address payable[] internal users;
 
-    function beforeEach() public override {
-        token = new ERC20FixedSupply();
-        owner = address(this);
+    address internal alice;
+    address internal bob;
+
+    function setUp() public virtual {
+        utils = new Utils();
+        users = utils.createUsers(5);
+
+        alice = users[0];
+        vm.label(alice, "Alice");
+        bob = users[1];
+        vm.label(bob, "Bob");
     }
+}
 
-    function testInitialize() public {
-        // Call initialize function
-        string memory name = "My Token";
-        string memory symbol = "MTK";
-        uint8 decimals = 18;
-        uint256 totalSupply = 1000000;
-        token.initialize(name, symbol, decimals, totalSupply);
-
-        // Verify token details
-        Assert.equal(token.name(), name, "Incorrect token name");
-        Assert.equal(token.symbol(), symbol, "Incorrect token symbol");
-        Assert.equal(token.decimals(), decimals, "Incorrect token decimals");
-        Assert.equal(token.totalSupply(), totalSupply * 10**uint256(decimals), "Incorrect token total supply");
-
-        // Verify owner balance
-        Assert.equal(token.balanceOf(owner), totalSupply * 10**uint256(decimals), "Incorrect owner balance");
-    }
+contract ERC20FixedSupplyTest is ERC20FixedSupplyBaseSetup {
 
     function testTransfer() public {
-        // Call initialize function
-        string memory name = "My Token";
-        string memory symbol = "MTK";
         uint8 decimals = 18;
         uint256 totalSupply = 1000000;
-        token.initialize(name, symbol, decimals, totalSupply);
-
         // Transfer tokens
         address recipient = address(0x123);
         uint256 amount = 1000;
         token.transfer(recipient, amount);
 
         // Verify balances
-        Assert.equal(token.balanceOf(owner), totalSupply * 10**uint256(decimals) - amount, "Incorrect owner balance");
-        Assert.equal(token.balanceOf(recipient), amount, "Incorrect recipient balance");
+        assertEq(token.balanceOf(owner), totalSupply * 10**uint256(decimals) - amount, "Incorrect owner balance");
+        assertEq(token.balanceOf(recipient), amount, "Incorrect recipient balance");
     }
 }
