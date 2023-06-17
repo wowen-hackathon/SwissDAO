ca94fd68420d678f4a86c4a69d6d2b973446c348
lukas <lukas@ibex.host>
2023-06-17 10:29:07 +0200

    Intermediary commit, have to uninstall a package.

--- contracts/test/ERC20FixedSupply.t.sol
+++ contracts/test/ERC20FixedSupply.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.19;
 
 import "@forge-std/Test.sol";
 import {console} from "@forge-std/console.sol";
@@ -8,35 +8,39 @@ import {stdStorage, StdStorage, Test} from "@forge-std/Test.sol";
 import "../src/erc20/ERC20FixedSupply.sol";
 
 contract ERC20FixedSupplyBaseSetup is ERC20FixedSupply, Test {
-    Utils internal utils;
-    address payable[] internal users;
-
     address internal alice;
     address internal bob;
+    
+    constructor() ERC20FixedSupply("Test Token Fixed", "TTF", 1000000) {
+
+    }
 
     function setUp() public virtual {
-        utils = new Utils();
-        users = utils.createUsers(5);
-
-        alice = users[0];
+        alice = vm.addr(1);
+        vm.deal(alice, 100 ether);
         vm.label(alice, "Alice");
-        bob = users[1];
+
+        bob = vm.addr(2);
+        vm.deal(bob, 100 ether);
         vm.label(bob, "Bob");
     }
 }
 
 contract ERC20FixedSupplyTest is ERC20FixedSupplyBaseSetup {
 
+    function setUp() public virtual override {
+        ERC20FixedSupplyBaseSetup.setUp();
+        console.log("When transferring tokens");
+    }
+
     function testTransfer() public {
-        uint8 decimals = 18;
-        uint256 totalSupply = 1000000;
         // Transfer tokens
-        address recipient = address(0x123);
         uint256 amount = 1000;
-        token.transfer(recipient, amount);
+
+        this.transfer(bob, amount);
 
         // Verify balances
-        assertEq(token.balanceOf(owner), totalSupply * 10**uint256(decimals) - amount, "Incorrect owner balance");
-        assertEq(token.balanceOf(recipient), amount, "Incorrect recipient balance");
+        assertEq(this.balanceOf(address(this)), totalSupply * 10**decimals() - amount);
+        assertEq(this.balanceOf(bob), amount, "Incorrect recipient balance");
     }
 }
